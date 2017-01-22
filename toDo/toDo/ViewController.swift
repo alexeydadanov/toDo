5d4f22971ddfff2b30e81378a2e2a99247674066
alexey_dadanov <dadanovalexey@yandex.ru>
2017-01-22 19:43:15 +0300

    add CoreData
    string save in memory iPhone (sqLite)

--- toDo/toDo/ViewController.swift
+++ toDo/toDo/ViewController.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    var items: [String] = []
+    var items: [NSManagedObject] = []
+    
     
     @IBOutlet weak var listTableView: UITableView!
     @IBAction func addItem(_ sender: Any) {
         alert()
+        
     }
     @IBAction func editTapped(_ sender: Any) {
        
@@ -45,7 +48,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let add = UIAlertAction(title: "add", style: .default) {
             (action) in
             let textField = alert.textFields![0] 
-            self.items.append(textField.text!)
+            self.save(name: textField.text!)
             self.listTableView.reloadData()
         }
         let cancel = UIAlertAction(title: "cancel", style: .cancel) {
@@ -63,7 +66,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell =  tableView.dequeueReusableCell(withIdentifier: "itemCell") as! itemTableViewCell
-        cell.itemLabel.text = items[indexPath.row]
+        let person = items[indexPath.row]
+        cell.itemLabel.text = person.value(forKey: "name") as? String
         
         return cell
     }
@@ -85,6 +89,46 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         return true
     }
+    
+    func save(name: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
+        
+        let person = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        person.setValue(name, forKeyPath: "name")
+        
+        do {
+            try managedContext.save()
+            items.append(person)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
+        do {
+            items = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
 }
+
 
 
