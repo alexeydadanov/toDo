7785bc248a5c884c9387c282800b442db7ae0ce9
alexey_dadanov <dadanovalexey@yandex.ru>
2017-01-24 21:35:45 +0300

    add textViewController
    - add action:
    
    When push to cell open new viewController

---
+++ toDo/toDo/textViewController.swift
@@ -0,0 +1,35 @@
+//
+//  textViewController.swift
+//  toDo
+//
+//  Created by Dadanov Alexey on 24.01.17.
+//  Copyright © 2017 Dadanov Alexey. All rights reserved.
+//
+
+import UIKit
+
+class textViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
